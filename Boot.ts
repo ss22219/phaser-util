5f74f31c5a7093ce7e838929e07a6ca3c28788fa
gool <ss22219@qq.com>
2014-11-23 18:00:42 +0800

    #5

--- Boot.ts
+++ Boot.ts
@@ -11,7 +11,7 @@ module Castlevania {
         create() {
 
             //  Unless you specifically need to support multitouch I would recommend setting this to 1
-            this.input.maxPointers = 1;
+            this.input.maxPointers = 2;
 
             //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
             //this.stage.disableVisibilityChange = true;
