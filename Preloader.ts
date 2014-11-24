312e2d546f0cf5b2be762ade8337e795e7865c9a
ss22219 <ss22219@qq.com>
2014-11-24 10:42:50 +0800

    #6

--- Preloader.ts
+++ Preloader.ts
@@ -4,16 +4,16 @@
         progress: RectangleProgress;
         preload() {
             this.load.image("background", "assets/background.jpg", true);
-            this.progress = new RectangleProgress(this, this.world.width * 0.05, this.world.height * 0.35, 10, this.world.width * 0.9);
+            this.progress = new RectangleProgress(this, this.world.width * 0.05, this.world.height * 0.4, 10, this.world.width * 0.9);
             var text = this.add.text(this.progress.x, this.progress.y - 24, "正在加载:", { fill: "#CDCDCD", font: "20px 新宋体" });
             this.load.onFileComplete.addOnce(() => {
                 this.progress.length = this.load.progress;
             }, this);
-            new PointLoadProgress(this, this.world.centerY);
+            new PointLoadProgress(this);
         }
 
         create() {
-            this.game.state.start("World1")
+            //this.game.state.start("World1")
         }
 
         update() {
